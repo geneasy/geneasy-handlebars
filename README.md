c1deea40693b1f17f415fa0440dbad76e60a75dd
pipecraft <pipecraft.net@gmail.com>
2021-09-21 05:11:10 +0800

    Implements Handlebars plugin

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # geneasy-handlebars
-A plugin that enables geneasy to use the Handlebars templates. 
+
+[![NPM Version][npm-version-image]][npm-url]
+[![LICENSE][license-image]][license-url]
+[![code style: prettier][code-style-prettier-image]][code-style-prettier-url]
+
+A plugin that enables geneasy to use the Handlebars templates.
+
+## Installation
+
+```sh
+npm i -g geneasy-handlebars
+```
+
+## Related
+
+- [geneasy](https://github.com/geneasy/geneasy) - A command line tool that can easily generate HTML, Markdown documents, etc.
+- [geneasy-yaml](https://github.com/geneasy/geneasy-yaml) - A plugin that enables geneasy to use dafa files in YAML format.
+- [handlebars.js](https://github.com/handlebars-lang/handlebars.js) - Minimal templating on steroids
+
+## License
+
+Copyright (c) 2021 [Pipecraft][my-url]. Licensed under the [MIT license][license-url].
+
+## >\_
+
+[![Pipecraft](https://img.shields.io/badge/site-pipecraft-brightgreen)](https://www.pipecraft.net)
+[![PZWD](https://img.shields.io/badge/site-pzwd-brightgreen)](https://pzwd.net)
+
+[my-url]: https://www.pipecraft.net
+[npm-url]: https://www.npmjs.com/package/geneasy-handlebars
+[license-url]: LICENSE
+[code-style-prettier-url]: https://github.com/prettier/prettier
+[npm-downloads-image]: https://img.shields.io/npm/dm/geneasy-handlebars
+[npm-version-image]: https://img.shields.io/npm/v/geneasy-handlebars
+[license-image]: https://img.shields.io/npm/l/geneasy-handlebars
+[code-style-prettier-image]: https://img.shields.io/badge/code_style-prettier-ff69b4.svg?style=flat-square
---
+++ test/index.js
@@ -0,0 +1,59 @@
+import { expect } from 'chai';
+import render, { register } from '../lib/index.js';
+
+describe('geneasy-handlebars', () => {
+  it('render should to be a function', () => {
+    expect(render).to.be.a('function');
+  });
+
+  it('should throw an error if template not provided', async () => {
+    try {
+      await render();
+      expect.fail('was not supposed to succeed');
+    } catch (error) {
+      expect(error).to.be.an('error');
+    }
+  });
+
+  it('should render template without data', async () => {
+    const result = await render('hello, {{what}}!');
+    expect(result).equals('hello, !');
+  });
+
+  it('should render template with data', async () => {
+    const result = await render('hello, {{what}}!', { what: 'world' });
+    expect(result).equals('hello, world!');
+  });
+
+  it('should render template with toLowerCase helper', async () => {
+    const result = await render('hello, {{toLowerCase what}}!', {
+      what: 'WORLD'
+    });
+    expect(result).equals('hello, world!');
+  });
+
+  it('should render template with toUpperCase helper', async () => {
+    const result = await render('hello, {{toUpperCase what}}!', {
+      what: 'world'
+    });
+    expect(result).equals('hello, WORLD!');
+  });
+
+  it('should render template with toTitleCase helper', async () => {
+    const result = await render('hello, {{toTitleCase what}}!', {
+      what: 'wonderful world'
+    });
+    expect(result).equals('hello, Wonderful World!');
+  });
+
+  it('should render template with toCamelCase helper', async () => {
+    const result = await render('hello, {{toCamelCase what}}!', {
+      what: 'wonderful-world'
+    });
+    expect(result).equals('hello, wonderfulWorld!');
+  });
+
+  it('register should to be a function', () => {
+    expect(register).to.be.a('function');
+  });
+});
